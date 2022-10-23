25e6d3e839fd66e17f2641fc0704f2f3beee2014
Turchanov Denis <turchanovdenis786@gmail.com>
2022-10-23 13:15:38 +0200

    update path

--- otaky-project/src/App.vue
+++ otaky-project/src/App.vue
@@ -18,5 +18,5 @@ export default {
 <style>
 @import "https://fonts.googleapis.com/css2?family=Cinzel&family=Rajdhani:wght@300;600&display=swap";
 @import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css";
-@import "../../src/css/style.css";
+@import "css/style.css";
 </style>
